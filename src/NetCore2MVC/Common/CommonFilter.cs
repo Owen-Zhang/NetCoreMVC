31c31b6fc0afdbd826baf63309bba0f8665c4c35
Owen <1034992601@qq.com>
2017-10-27 16:37:58 +0800

    filter

---
+++ src/NetCore2MVC/Common/CommonFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace NetCore2MVC.Common
+{
+    [AttributeUsage(AttributeTargets.All, Inherited =true, AllowMultiple=true )]
+    public class CommonFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var culture = "zh-CN";
+            var cultureInput = filterContext.HttpContext.Request.Headers["culture"];
+
+            switch (cultureInput)
+            {
+                case "zh-HK":
+                case "zh-TW":
+                    culture = "zh-HK";
+                    break;
+
+                default:
+                    culture = "zh-CN";
+                    break;
+            }
+            System.Threading.Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo(culture); //en-Us, zh-HK,zh-CN,zh-TW
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
